df0470f03a1abc531cb3f274ff69ef042aaf4bc7
Carlos Shaka <carlos.shaka02@gmail.com>
2016-07-20 08:32:20 -0300

    Coment send email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -27,7 +27,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      ContactMailer.confirm_email(@contact).deliver_now
+      # ContactMailer.confirm_email(@contact).deliver_now
       render json: @contact
     else
       render json: @contact.errors
