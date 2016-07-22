f5c54a223b983397f827e1a22f3ba93d653d2bc3
Carlos Shaka <carlos.shaka02@gmail.com>
2016-07-22 09:02:03 -0300

    Change propose to proposal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts
-  get 'propose', to: 'home#propose'
+  get 'proposal', to: 'home#proposal'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
